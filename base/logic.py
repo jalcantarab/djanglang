1c081cc5a45512c266eb8ef67fe39b7f7e094343
jalcantarab <jorgeakairos@gmail.com>
2024-02-11 22:52:24 -0800

    Updated Libraries & Fixed issues
    Should be ready for djanglang article

--- base/logic.py
+++ base/logic.py
@@ -1,38 +1,26 @@
-from urllib.parse import urlparse
-from pathlib import Path
 from bs4 import BeautifulSoup
-import requests
-from langchain.document_loaders import WebBaseLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.chat_models import ChatOpenAI
-from typing import Union, Any
 from dotenv import load_dotenv
+from pathlib import Path
+from urllib.parse import urlparse
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from typing import Union, Optional, Any
+import requests
 import os
 
 load_dotenv()
 
-CHROMA_DB_DIRECTORY = "chroma_db/ask_bootstrap_docs"
-
-from bs4 import BeautifulSoup
+CHROMA_DB_DIRECTORY = "chroma_db/bootstrap_docs"
 
 class CustomWebBaseLoader(WebBaseLoader):
-    
-    def custom_bs4(self, soup):
-        print("====WE'RE PARSING==============")
-        """
-        Custom BeautifulSoup parser to extract content within the <main> tag.
-        """        
-        main_tag = soup.find('main') # Find the <main> tag
-        return BeautifulSoup(main_tag.text, 'html.parser') if main_tag else ""
-
-
-    def _scrape(self, url: str, parser: Union[str, None] = None) -> Any:
-        print("====WE'RE SCRAPING==============")
+    def _scrape(self, url: str, parser: Union[str, None] = None, bs_kwargs: Optional[dict] = None) -> Any:
         html_content = super()._scrape(url, parser)
-        return self.custom_bs4(html_content)
+        main_tag = html_content.find('main')  # Find the <main> tag
+        return BeautifulSoup(main_tag.text, "html.parser", **(bs_kwargs or {}))
 
 def database_exists():
     return os.path.exists(CHROMA_DB_DIRECTORY)
@@ -47,11 +35,11 @@ def bootstrap_docs_build_urls():
     root_links = soup.find_all("a", attrs={"class": "bd-links-link d-inline-block rounded"})
 
     result = set()
-    # limit = 4
-    # counter = 0
+    limit = 4
+    counter = 0
     for root_link in root_links:
-        # if counter > limit: 
-        #     break
+        if counter > limit: 
+            break
         counter=counter+1
         path = root_link.get("href")
         path = str(Path(path).resolve())
@@ -71,20 +59,20 @@ def build_database():
     loader = CustomWebBaseLoader(urls)
     documents = loader.load()
     print("documents")
-    # print(documents)
-    text_splitter = CharacterTextSplitter(
-        separator = "\n\n",
-        chunk_size=750,
-        chunk_overlap=0
-    )
-    splitted_documents = text_splitter.split_documents(documents)
-
+    print(documents)
+    splitter =RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=1000, chunk_overlap=0)
+    splits = splitter.split_documents(documents)
+    # text_splitter = CharacterTextSplitter(
+    #     separator = "\n\n",
+    #     chunk_size=750,
+    #     chunk_overlap=0
+    # )
     embeddings = OpenAIEmbeddings()
 
     db = Chroma.from_documents(
-        splitted_documents,
+        splits,
         embeddings,
-        collection_name="ask_bootstrap_docs",
+        collection_name="bootstrap_docs",
         persist_directory=CHROMA_DB_DIRECTORY,
     )
     db.persist()
@@ -92,7 +80,7 @@ def build_database():
 def answer_query(query):
     embeddings = OpenAIEmbeddings()
     db = Chroma(
-        collection_name="ask_django_docs",
+        collection_name="bootstrap_docs",
         embedding_function=embeddings,
         persist_directory=CHROMA_DB_DIRECTORY
     )
